ae860b10cc3aa50d96b41aff499d5792d0a22ae7
ryzxxnnx-blip <ryzxxnnx@gmail.com>
2026-01-28 15:09:01 +0700

    Create db.php

---
+++ config/db.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+$config = require __DIR__ . '/config.php';
+$db = $config['db'];
+
+$dsn = sprintf('mysql:host=%s;dbname=%s;charset=%s', $db['host'], $db['name'], $db['charset']);
+
+$pdo = new PDO(
+    $dsn,
+    $db['user'],
+    $db['pass'],
+    [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]
+);
