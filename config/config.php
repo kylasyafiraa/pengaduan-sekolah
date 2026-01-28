9a50e40ae9eabc7f8c65064259935051e495ead9
ryzxxnnx-blip <ryzxxnnx@gmail.com>
2026-01-28 15:08:54 +0700

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'base_url' => '/pengaduan%20sekolah',
+    'db' => [
+        'host' => '127.0.0.1',
+        'name' => 'pengaduan_sekolah',
+        'user' => 'root',
+        'pass' => '',
+        'charset' => 'utf8mb4',
+    ],
+];
